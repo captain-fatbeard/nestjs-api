511bb8e117be5a077d1dd818f7cb67dc8052de5f
Jacob Juul Larsen <jacoblarsen86@live.com>
2022-06-08 15:50:52 +0200

    bug: fix import routes for testing

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { UserController } from './user/user.controller';
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
-import { UserDecorator } from 'src/auth/decorator';
-import { JwtGuard } from 'src/auth/guard';
+import { UserDecorator } from '../auth/decorator';
+import { JwtGuard } from '../auth/guard';
 
 @UseGuards(JwtGuard)
 @Controller('users')
