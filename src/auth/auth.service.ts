9827ff1a266f04e2de261bfa397aa2d27c4a7002
Jacob Juul Larsen <jacoblarsen86@live.com>
2022-06-08 10:00:12 +0200

    feat: add register user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import * as argon from 'argon2';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 
@@ -6,8 +8,27 @@ import { AuthDto } from './dto';
 export class AuthService {
     constructor(private prisma: PrismaService) {}
 
-    register(dto: AuthDto) {
-        return { msg: 'user is signed up' };
+    async register(dto: AuthDto) {
+        const hash = await argon.hash(dto.password);
+
+        try {
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash,
+                    name: dto.name,
+                },
+            });
+            delete user.hash;
+
+            return user;
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
+                    throw new ForbiddenException('Duplicate user');
+                }
+            }
+        }
     }
 
     login() {
