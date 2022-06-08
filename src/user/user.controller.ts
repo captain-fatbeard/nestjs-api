d04481dd01d582f2f4da4dc08c6f703ed0156c68
Jacob Juul Larsen <jacoblarsen86@live.com>
2022-06-08 15:25:49 +0200

    feat: add custom AuthGuard and UserDecorator

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-import { Request } from 'express';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { UserDecorator } from 'src/auth/decorator';
+import { JwtGuard } from 'src/auth/guard';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
-    @UseGuards(AuthGuard('jwt-access'))
     @Get('me')
-    me(@Req() req: Request) {
-        return req.user;
+    me(@UserDecorator() user: User) {
+        return user;
     }
 }
